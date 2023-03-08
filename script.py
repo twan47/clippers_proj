c74de62ead225d78abc9db68ac8a6241ae4a5184
Twan47 <anthonynewell171@gmail.com>
2023-03-08 08:42:10 -0500

    added first loop

--- script.py
+++ script.py
@@ -3,3 +3,8 @@ hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk"
 prices = [30, 25, 40, 20, 20, 35, 50, 35]
 
 last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+
+total_price = 0
+
+for price in prices:
+  total_price = total_price + price
