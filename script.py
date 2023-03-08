83fd9b2e0b4e9d66d006d4dd183e5d67319cc83f
Twan47 <anthonynewell171@gmail.com>
2023-03-08 13:18:25 -0500

    added last look for cuts under 30 printed. Project finished!

--- script.py
+++ script.py
@@ -18,3 +18,9 @@ total_revenue = 0
 for i in range(len(hairstyles)):
   total_revenue += prices[i] * last_week[i]
 print("Total Revenue: ${0}".format(total_revenue))
+
+average_daily_revenue = total_revenue / 7
+print("Average Daily Revenue: ${0}".format(average_daily_revenue))
+
+cuts_under_30 = [hairstyles [i] for i in range(len(hairstyles)) if new_prices[i] < 30]
+print(cuts_under_30)
